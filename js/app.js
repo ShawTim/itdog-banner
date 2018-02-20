0f42090f206f288b3be7318369b673ec14df7db2
ShawTim <shawtim.liu@gmail.com>
2018-02-21 04:57:24 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import '../css/banner.css';
+import 'image-picker/image-picker/image-picker.css';
+
+import 'image-picker';
+import 'html2canvas';
+import './main';
---
+++ js/main.js
@@ -0,0 +1,169 @@
+import html2canvas from 'html2canvas';
+import interact from 'interactjs';
+import tippy from 'tippy.js';
+
+const convertImage = () => {
+  html2canvas($(".image-container").get(0)).then((canvas) => $(".download-link").attr("href", canvas.toDataURL("image/png")));
+};
+
+const getCenter = () => {
+  const center = $(".banner-center-center");
+  const offset = center.offset();
+  const width = center.width();
+  const height = center.height();
+  return { x: offset.left + width/2, y: offset.top + height/2 };
+}
+
+const setBannerTextStyle = () => {
+  const container = $(".banner-container");
+  const banner = $(".banner-text");
+  const text = banner.text();
+  const maxW = container.width() * 0.9;
+  const maxH = container.height() * 0.9;
+  const { x, y } = getCenter();
+
+  const div = $("<div></div>").css("font-size", "1em").addClass("text-font").text(text);
+  $(document.body).append(div);
+  const w = div.width();
+  const h = div.height();
+  div.remove();
+
+  banner.css({
+    top: y,
+    left: x,
+    fontSize: `${Math.min(maxW/w, maxH/h)}em`,
+  });
+}
+
+const addSticker = (url) => {
+  const container = $(".image-container");
+  const image = $("<img/>").attr({ src: url, tabIndex: 1 }).css({ height: 100, "z-index": 10, position: "absolute" });
+  container.prepend(image);
+  setDraggable(image.get(0));
+  setResizable(image.get(0), { preserveAspectRatio: true, useTranslate: true });
+  image.mouseover(function(e) { $(this).focus(); });
+  image.keyup(function(e) {
+    console.log(e.keyCode)
+    if (e.keyCode === 27 || e.keyCode === 8 || e.keyCode === 46) {
+      $(this).remove();
+    }
+  });
+}
+
+const setDraggable = (element, options = {}) => interact(element).draggable($.extend(true, {}, {
+  restrict: {
+    restriction: "parent",
+    endOnly: false,
+  },
+  onmove: (e) => {
+    const ele = $(e.target);
+    const x = parseFloat(ele.attr("data-x") || 0) + e.dx;
+    const y = parseFloat(ele.attr("data-y") || 0) + e.dy;
+    ele.css({
+      transform: `translate(${x}px, ${y}px)`
+    }).attr({
+      "data-x": x,
+      "data-y": y,
+    })
+  },
+  onend: () => convertImage(),
+}, options));
+
+const setResizable = (element, options = {}) => interact(element).resizable($.extend(true, {}, {
+  edges: {
+    left: true,
+    right: true,
+    bottom: true,
+    top: true,
+  },
+  restrictEdges: {
+    outer: "parent",
+    endOnly: false,
+  },
+  restrictSize: {
+    min: { width: 50, height: 50 },
+  },
+}, options)).on("resizemove", (e) => {
+  const ele = $(e.target);
+  const x = parseFloat(ele.attr("data-x") || 0) + e.deltaRect.left;
+  const y = parseFloat(ele.attr("data-y") || 0) + e.deltaRect.top;
+  ele.css(options.useTranslate ? {
+    width: `${e.rect.width}px`,
+    height: `${e.rect.height}px`,
+    transform: `translate(${x}px, ${y}px)`,
+  } : {
+    width: `${e.rect.width+Math.abs(x)}px`,
+    height: `${e.rect.height+Math.abs(y)}px`,
+  }).attr({
+    "data-x": x,
+    "data-y": y,
+  });
+
+  setBannerTextStyle();
+}).on("resizeend", (e) => {
+  convertImage();
+});
+
+$(function() {
+  setResizable(".banner-container", {
+    restrictEdges: {
+      endOnly: true,
+    },
+    restrictSize: {
+      min: { width: 320, height: 320 },
+    },
+  });
+
+  $(".text-picker input").keyup((e) => {
+    const text = e.target.value;
+    $(".banner-text").text(text || "");
+    setBannerTextStyle();
+    convertImage();
+  })
+
+  $(".sticker-picker select").val("0").imagepicker({
+    initialized: () => tippy(".sticker-picker .image_picker_selector img", {
+      placement: "bottom",
+      arrow: true,
+      size: "small",
+      distance: 60,
+      html: "#sticker-tooltip",
+    }),
+    clicked: (select, e) => addSticker($(e.target).attr("src")),
+  });
+
+  tippy(".text-picker input", {
+    placement: "top",
+    arrow: true,
+    size: "small",
+    distance: 10,
+    html: "#text-tooltip",
+  });
+
+  tippy(".banner-container", {
+    placement: "left",
+    arrow: true,
+    size: "small",
+    distance: 10,
+    html: "#banner-tooltip",
+  });
+
+  tippy(".credit img", {
+    placement: "top",
+    arrow: true,
+    size: "small",
+    distance: 10,
+    html: "#credit-tooltip",
+  });
+
+  tippy(".download-link", {
+    placement: "top",
+    arrow: true,
+    size: "small",
+    distance: 10,
+    html: "#download-tooltip",
+  });
+
+  setBannerTextStyle();
+  convertImage();
+});
